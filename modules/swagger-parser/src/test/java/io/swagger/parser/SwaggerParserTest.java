ca624ada7927480c182395aec853d9a24cd75f87
Fuzail Sarang <fuzail@fingerprintsoft.co>
2021-04-14 13:10:51 +0200

    Due to classloader issues iliad need an update of 1.0.37 tag.

--- modules/swagger-parser/src/test/java/io/swagger/parser/SwaggerParserTest.java
+++ modules/swagger-parser/src/test/java/io/swagger/parser/SwaggerParserTest.java
@@ -1261,5 +1261,15 @@ public class SwaggerParserTest {
         assertEquals("k-definition", definitions.get("k_2").getTitle());
     }
 
+    @Test
+    public void testIssue1552() {
+        Swagger swagger = new SwaggerParser().read("src/test/resources/issue1552.yaml");
+
+        Assert.assertNotNull(swagger);
+        Assert.assertNotNull(swagger.getResponses().get("Response"));
+        Assert.assertNotNull(swagger.getResponses().get("Response").getResponseSchema().getProperties());
+        Assert.assertNotNull(swagger.getResponses().get("Response").getResponseSchema().getProperties().get("Report"));
+
+    }
 
 }
